7488046f0c01dde7b939b336502c4f6aee306a5b
gokulakrishnaa <gokulakrishnaa.p@telliant.net>
2024-03-27 11:37:27 +0530

    Added heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Git - Pull Request</h2>
+    </div>
+  );
 }
 
 export default App;
